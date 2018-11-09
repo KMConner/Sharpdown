f5fa4301225b6a2fa2a444e02a062bf009eed13b
KMConner <KMConner@users.noreply.github.com>
2018-11-09 18:15:40 +0900

    Added classes of inline elements

---
+++ Sharpdown/MarkdownElement/InlineElement/InlineElementBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpdown.MarkdownElement.InlineElement
+{
+    public class InlineElementBase : MarkdownElementBase
+    {
+
+    }
+}
