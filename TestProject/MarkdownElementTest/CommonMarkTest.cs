16b5e758f693c3ad13eb4ab8b97b874e9abb3cd3
KMConner <KMConner@users.noreply.github.com>
2019-10-29 00:08:29 +0900

    Updated test cases for link reference definitions
    Also fixed bugs in parsing

--- TestProject/MarkdownElementTest/CommonMarkTest.cs
+++ TestProject/MarkdownElementTest/CommonMarkTest.cs
@@ -2225,7 +2225,7 @@ namespace TestProject.MarkdownElementTest
         #region Link reference definitions
 
         [TestMethod]
-        public void TestCase_159()
+        public void TestCase_161()
         {
             const string html = "[foo]: /url \"title\"\r\n\r\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2243,7 +2243,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_160()
+        public void TestCase_162()
         {
             const string html = "   [foo]: \r\n      /url  \r\n           'the title'  \r\n\r\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2261,7 +2261,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_161()
+        public void TestCase_163()
         {
             const string html = "[Foo*bar\\]]:my_(url) 'title (with parens)'\r\n\r\n[Foo*bar\\]]";
             var doc = new MarkdownParser().Parse(html);
@@ -2279,7 +2279,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_162()
+        public void TestCase_164()
         {
             const string html = "[Foo bar]:\r\n<my%20url>\r\n'title'\r\n\r\n[Foo bar]";
             var doc = new MarkdownParser().Parse(html);
@@ -2297,7 +2297,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_163()
+        public void TestCase_165()
         {
             const string html = "[foo]: /url '\r\ntitle\r\nline1\r\nline2\r\n'\r\n\r\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2318,7 +2318,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_164()
+        public void TestCase_166()
         {
             const string html = "[foo]: /url 'title\r\n\r\nwith blank line'\r\n\r\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2336,7 +2336,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_165()
+        public void TestCase_167()
         {
             const string html = "[foo]:\r\n/url\r\n\r\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2356,7 +2356,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_166()
+        public void TestCase_168()
         {
             const string html = "[foo]:\n\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2372,7 +2372,46 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_167()
+        public void TestCase_169()
+        {
+            const string html = "[foo]: <>\n\n[foo]";
+            var doc = new MarkdownParser().Parse(html);
+            var label = "foo";
+            var dest = "";
+            string title = null;
+            Assert.AreEqual(1, doc.Elements.Count);
+            Assert.AreEqual(1, doc.LinkDefinition.Count);
+            Assert.AreEqual(label, doc.LinkDefinition[label].Label);
+            Assert.AreEqual(dest, doc.LinkDefinition[label].Destination, true);
+            Assert.AreEqual(title, doc.LinkDefinition[label].Title);
+            Assert.AreEqual(BlockElementType.Paragraph, doc.Elements[0].Type);
+            var inline = new InlineStructure(InlineElementType.Link,
+                new InlineStructure(InlineElementType.InlineText, label));
+            inline.AssertEqual(doc.Elements[0].GetInlines());
+            Assert.AreEqual(dest, (doc.Elements[0].GetInline(0) as Link).Destination, true);
+            Assert.AreEqual(title, (doc.Elements[0].GetInline(0) as Link).Title);
+        }
+
+        [TestMethod]
+        public void TestCase_170()
+        {
+            const string html = "[foo]: <bar>(baz)\n\n[foo]";
+            var doc = new MarkdownParser().Parse(html);
+            Assert.AreEqual(2, doc.Elements.Count);
+            Assert.AreEqual(0, doc.LinkDefinition.Count);
+            Assert.AreEqual(BlockElementType.Paragraph, doc.Elements[0].Type);
+            var inline = new InlineStructure(
+                new InlineStructure(InlineElementType.InlineText, "[foo]: "),
+                new InlineStructure(InlineElementType.InlineHtml, "<bar>"),
+                new InlineStructure(InlineElementType.InlineText, "(baz)"));
+            inline.AssertEqual(doc.Elements[0].GetInlines());
+
+            var inline2 = new InlineStructure(InlineElementType.InlineText, "[foo]");
+            inline2.AssertEqual(doc.Elements[1].GetInlines());
+        }
+
+        [TestMethod]
+        public void TestCase_171()
         {
             const string html = "[foo]: /url\\bar\\*baz \"foo\\\"bar\\baz\"\r\n\r\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2393,7 +2432,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_168()
+        public void TestCase_172()
         {
             const string html = "[foo]\r\n\r\n[foo]: url";
             var doc = new MarkdownParser().Parse(html);
@@ -2413,7 +2452,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_169()
+        public void TestCase_173()
         {
             const string html = "[foo]\r\n\r\n[foo]: first\r\n[foo]: second";
             var doc = new MarkdownParser().Parse(html);
@@ -2433,7 +2472,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_170()
+        public void TestCase_174()
         {
             const string html = "[FOO]: /url\r\n\r\n[Foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2453,7 +2492,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_171()
+        public void TestCase_175()
         {
             const string html = "[ΑΓΩ]: /φου\r\n\r\n[αγω]";
             var doc = new MarkdownParser().Parse(html);
@@ -2473,7 +2512,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_172()
+        public void TestCase_176()
         {
             const string html = "[foo]: /url";
             var doc = new MarkdownParser().Parse(html);
@@ -2487,7 +2526,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_173()
+        public void TestCase_177()
         {
             const string html = "[\r\nfoo\r\n]: /url\r\nbar";
             var doc = new MarkdownParser().Parse(html);
@@ -2504,7 +2543,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_174()
+        public void TestCase_178()
         {
             const string html = "[foo]: /url \"title\" ok";
             var doc = new MarkdownParser().Parse(html);
@@ -2516,7 +2555,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_175()
+        public void TestCase_179()
         {
             const string html = "[foo]: /url\r\n\"title\" ok";
             var doc = new MarkdownParser().Parse(html);
@@ -2533,7 +2572,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_176()
+        public void TestCase_180()
         {
             const string html = "    [foo]: /url \"title\"\r\n\r\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2547,7 +2586,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_177()
+        public void TestCase_181()
         {
             const string html = "```\r\n[foo]: /url\r\n```\r\n\r\n[foo]";
             var doc = new MarkdownParser().Parse(html);
@@ -2561,7 +2600,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_178()
+        public void TestCase_182()
         {
             const string html = "Foo\r\n[bar]: /baz\r\n\r\n[bar]";
             var doc = new MarkdownParser().Parse(html);
@@ -2579,7 +2618,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_179()
+        public void TestCase_183()
         {
             const string html = "# [Foo]\r\n[foo]: /url\r\n> bar";
             var doc = new MarkdownParser().Parse(html);
@@ -2603,7 +2642,56 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_180()
+        public void TestCase_184()
+        {
+            const string html = "[foo]: /url\nbar\n===\n[foo]";
+            var doc = new MarkdownParser().Parse(html);
+            var label = "foo";
+            var dest = "/url";
+            Assert.AreEqual(2, doc.Elements.Count);
+            Assert.AreEqual(1, doc.LinkDefinition.Count);
+            Assert.AreEqual(label, doc.LinkDefinition[label].Label, true);
+            Assert.AreEqual(dest, doc.LinkDefinition[label].Destination);
+            Assert.AreEqual(null, doc.LinkDefinition[label].Title);
+            Assert.AreEqual(BlockElementType.Heading, doc.Elements[0].Type);
+            Assert.AreEqual(BlockElementType.Paragraph, doc.Elements[1].Type);
+            Assert.AreEqual(1, (doc.Elements[0] as Heading).HeaderLevel);
+
+            var inline0 = new InlineStructure(InlineElementType.InlineText,"bar");
+            inline0.AssertEqual(doc.Elements[0].GetInlines());
+
+            var inline1 = new InlineStructure(InlineElementType.Link,
+                    new InlineStructure(InlineElementType.InlineText, "foo"));
+            inline1.AssertEqual(doc.Elements[1].GetInlines());
+            Assert.AreEqual(dest, (doc.Elements[1].GetInline(0) as Link).Destination);
+            Assert.AreEqual(null, (doc.Elements[1].GetInline(0) as Link).Title);
+        }
+
+        [TestMethod]
+        public void TestCase_185()
+        {
+            const string html = "[foo]: /url\n===\n[foo]";
+            var doc = new MarkdownParser().Parse(html);
+            var label = "foo";
+            var dest = "/url";
+            Assert.AreEqual(1, doc.Elements.Count);
+            Assert.AreEqual(1, doc.LinkDefinition.Count);
+            Assert.AreEqual(label, doc.LinkDefinition[label].Label, true);
+            Assert.AreEqual(dest, doc.LinkDefinition[label].Destination);
+            Assert.AreEqual(null, doc.LinkDefinition[label].Title);
+            Assert.AreEqual(BlockElementType.Paragraph, doc.Elements[0].Type);
+            var inline0 = new InlineStructure(
+                new InlineStructure(InlineElementType.InlineText, "==="),
+                new InlineStructure(InlineElementType.SoftLineBreak),
+                new InlineStructure(InlineElementType.Link,
+                    new InlineStructure(InlineElementType.InlineText, "foo")));
+            inline0.AssertEqual(doc.Elements[0].GetInlines());
+            Assert.AreEqual(dest, (doc.Elements[0].GetInline(2) as Link).Destination);
+            Assert.AreEqual(null, (doc.Elements[0].GetInline(2) as Link).Title);
+        }
+
+        [TestMethod]
+        public void TestCase_186()
         {
             const string html = "[foo]: /foo-url \"foo\"\r\n[bar]: /bar-url\r\n  \"bar\"\r\n" +
                                 "[baz]: /baz-url\r\n\r\n[foo],\r\n[bar],\r\n[baz]";
@@ -2636,7 +2724,7 @@ namespace TestProject.MarkdownElementTest
         }
 
         [TestMethod]
-        public void TestCase_181()
+        public void TestCase_187()
         {
             const string html = "[foo]\r\n\r\n> [foo]: /url";
             var doc = new MarkdownParser().Parse(html);
@@ -2651,6 +2739,18 @@ namespace TestProject.MarkdownElementTest
                 new InlineStructure(InlineElementType.InlineText, "foo"));
             inline0.AssertEqual(doc.Elements[0].GetInlines());
             Assert.AreEqual(0, doc.Elements[1].GetChildren().Count);
+        }
+
+        [TestMethod]
+        public void TestCase_188()
+        {
+            const string html = "[foo]: /url";
+            var doc = new MarkdownParser().Parse(html);
+            Assert.AreEqual(0, doc.Elements.Count);
+            Assert.AreEqual(1, doc.LinkDefinition.Count);
+            Assert.AreEqual("foo", doc.LinkDefinition["foo"].Label);
+            Assert.AreEqual("/url", doc.LinkDefinition["foo"].Destination);
+            Assert.AreEqual(null, doc.LinkDefinition["foo"].Title);
         }
 
         #endregion
